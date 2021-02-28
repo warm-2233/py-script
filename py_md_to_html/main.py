7c41a647fed04ea4d1d3848564bafd95f1790905
暗天使 <1248078323@qq.com>
2021-03-01 00:10:22 +0800

    2021年3月1日

--- py_md_to_html/main.py
+++ py_md_to_html/main.py
@@ -7,6 +7,12 @@ html_body_file = open("some_file.md","r",encoding='utf-8')
 html_body_txt = html_body_file.read()
 html_body_file.close()
 
+
+
+
+
+
+
 # 所支持的复杂元素
 exts = ['markdown.extensions.extra', 'markdown.extensions.codehilite','markdown.extensions.tables','markdown.extensions.toc',MathExtension(enable_dollar_delimiter=True)]
 
@@ -17,6 +23,16 @@ html = html_body
 html_file = open("file.html","w",encoding='utf-8')
 html_file.write(html)
 html_file.close()
+
+
+
+
+
+
+
+
+
+
 
 """
 import markdown
